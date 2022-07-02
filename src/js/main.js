c186ef6d14ee4f79663bf6e0d681070928626dbc
Bartosz Jędrzejewski-Emche <jedrek2603@gmail.com>
2022-07-02 20:23:55 +0200

    Add scroll spy functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,25 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav ul a')
+
+window.onscroll = () => {
+  section.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      })
+    }
+  })
+}
+
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
